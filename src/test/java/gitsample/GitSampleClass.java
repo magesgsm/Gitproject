5d0f655b05cb34296deba872335e7ca656df7898
magesgsm <magesgsm@gmail.com>
2021-04-09 11:46:29 +0530

    new

---
+++ src/test/java/gitsample/GitSampleClass.java
@@ -0,0 +1,15 @@
+package gitsample;
+
+public class GitSampleClass {
+	private void sample1() {
+		System.out.println("sample1");
+
+	}
+	private void sample2() {
+		System.out.println("sample2");
+	}
+	private void sample3() {
+		System.out.println("sample3");
+	}
+
+}

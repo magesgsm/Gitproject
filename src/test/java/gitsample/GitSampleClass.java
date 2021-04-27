457ba7ada42f9e57186e1f6317ea304550d72528
radha27121989 <radhamechmohan.rm@gmail.com>
2021-04-27 14:32:17 +0530

    addededed

--- src/test/java/gitsample/GitSampleClass.java
+++ src/test/java/gitsample/GitSampleClass.java
@@ -1,36 +1,42 @@
 package gitsample;
 
 public class GitSampleClass {
-	
+
 	private void sample1() {
 		System.out.println("sample1");
 
 	}
+
 	private void sample2() {
 		System.out.println("sample2");
 	}
+
 	private void sample3() {
 		System.out.println("sample3");
 	}
+
 	private void sample4() {
 		System.out.println("sample4");
 
 	}
+
 	private void sample5() {
 		System.out.println("sample5");
 	}
+
 	private void sample6() {
 		System.out.println("sample6");
 	}
 
-
 	private void sample7() {
 		System.out.println("sample7");
 	}
+
 	private void sample8() {
 		System.out.println("sample8");
 
 	}
+
 	private void sample9() {
 		System.out.println("sample9");
 
@@ -52,16 +58,24 @@ public class GitSampleClass {
 	}
 
 	private void sample01() {
-	System.out.println("srini");
+		System.out.println("srini");
 
 	}
+
 	private void sample02() {
 		System.out.println("srinivasan");
 	}
+
 	private void sample03() {
 		System.out.println("srinivasan2592");
 	}
 
-	
-	
+	private void sample04() {
+		System.out.println("radha");
+	}
+
+	private void sample05() {
+		System.out.println("radhagit");
+	}
+
 }
